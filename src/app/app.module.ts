b2be16fa96db9502f471b489f69b5ae81e4daacd
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2023-11-10 10:20:34 -0300

    added styles and html to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ItemsListComponent } from './shopping-list/items-list/items-list.component';
-import { ItemsEditComponent } from './shopping-list/items-edit/items-edit.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingEditComponent } from './shoping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ItemsListComponent,
-    ItemsEditComponent,
-    RecipeBookComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
+    ShoppingEditComponent,
     RecipesComponent,
-    ShoppingEditComponent
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
